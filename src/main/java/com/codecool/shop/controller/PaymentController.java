7914ac98e8722e4f10c417fc1518a9bc5debcbf1
Marius Ceobanu <ceobanu.marius@gmail.com>
2020-10-27 17:23:33 +0100

    Implement index page for card payment

---
+++ src/main/java/com/codecool/shop/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/checkout/payment"})
+public class PaymentController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CartDao cartDataStore = CartDaoMem.getInstance();
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        context.setVariable("cart", cartDataStore.find(0)); // TODO Replace with actual userId
+
+        engine.process("payment/index.html", context, resp.getWriter());
+    }
+}
